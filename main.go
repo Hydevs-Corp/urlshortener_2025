27491b182390de87ac67b628021a9168518bde2e
LouisRvl <91453783+LouisRvlE@users.noreply.github.com>
2025-11-07 10:31:56 +0100

    Implement Cobra root command and main execution

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"log"
+
+	"github.com/axellelanca/urlshortener/cmd"
 	_ "github.com/axellelanca/urlshortener/cmd/cli"    // Importe le package 'cli' pour que ses init() soient exécutés
 	_ "github.com/axellelanca/urlshortener/cmd/server" // Importe le package 'server' pour que ses init() soient exécutés
 )
 
 func main() {
-	// TODO
+	// Appelle le point d'entrée Cobra défini dans `cmd`.
+	defer func() {
+		if r := recover(); r != nil {
+			log.Fatalf("panic: %v", r)
+		}
+	}()
+
+	cmd.Execute()
 }
